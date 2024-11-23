808dd9aa2b567eaf3bfcd1be0986e3ca96cd9d28
Homare3 <tuturi.12145@gmail.com>
2024-11-24 00:16:29 +0900

    egu

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,1 @@
 pub mod library;
-
-fn function_1() {
-    let shelf = crate::library::bookshelf::Bookshelf::new();
-}
-
-fn function_2() {
-    use library::bookshelf;
-    let shelf = bookshelf::Bookshelf::new();
-}
